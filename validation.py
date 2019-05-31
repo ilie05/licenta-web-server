b6ce39a12f56b93bb1955876ba321a4ab8705650
yoki <petras.moil@gmail.com>
2019-05-31 14:56:02 +0300

    domain validation and throw exceptions

--- validation.py
+++ validation.py
@@ -2,11 +2,17 @@ import re
 
 REGEX_email = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
 
+# max. 63 characters; must not start or end with hyphen
+REGEX_label = r'[a-zA-Z90-9]([a-zA-Z90-9-]{0,61}[a-zA-Z90-9])?'
+
 
 class Validation(object):
-    def validate_email(self, email: str, domain: str = '') -> str:
+    @staticmethod
+    def check_email(email: str, domain: str = '') -> str:
+        email = str(email)
+        original_email = email
         if email == '':
-            return None
+            raise Exception("Admin email address must not be empty! ")
 
         regex_res = re.match(REGEX_email, email)
         if domain == '':
@@ -14,7 +20,7 @@ class Validation(object):
                 if regex_res:
                     return email
                 else:
-                    return None
+                    raise Exception("\'{}\' is not a valid email address".format(original_email))
             else:
                 return email
 
@@ -26,10 +32,29 @@ class Validation(object):
                 else:
                     return '.'.join(email)
             else:
-                return None
+                raise Exception("\'{}\' is not a valid email address".format(original_email))
         return str(''.join(email))
 
+    @staticmethod
+    def check_domain_name(name: str) -> str:
+        name = str(name)
+        pattern = r'^{0}(\.{0})*\.?$'.format(REGEX_label)
+        if re.match(pattern, name):
+            return name
+
+        raise Exception("\'{}\' is not a valid domain name".format(name))
+
+
 if __name__ == '__main__':
-    validation = Validation()
-    res = validation.validate_email('', 'gmail.com')
-    print(res)
+    try:
+        res1 = Validation.check_email('mai@l')
+        print(res1)
+    except Exception as e:
+        print(str(e))
+
+    try:
+        res2 = Validation.check_domain_name('a@sds.ds')
+        print(res2)
+    except Exception as e:
+        print(str(e))
+
