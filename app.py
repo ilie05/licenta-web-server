71d11b70800209de51f2a1a7256ca936bfb85194
yoki <petras.moil@gmail.com>
2019-06-05 00:21:42 +0300

    syntax errors

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import re
+import json
+import sys
 import pymongo
 import datetime
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["licenta"]
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client['licenta']
 collection = db['zones']
 
 app = Flask(__name__)
@@ -26,18 +29,44 @@ def record():
     return render_template("inserted.html", message=message)
 
 
+@app.route('/test')
+def test():
+    return render_template("test.html")
+
+
+@app.route('/dream', methods=['POST', 'GET'])
+def submited():
+    if request.method == 'POST':
+        # name = request.form.to_dict()['name']
+        # print(request.form.to_dict())
+        #data = json.loads(request.data)
+        print(request.get_json())
+        return render_template("dream.html", message='some message!!!!!')
+    else:
+        return redirect(url_for('test'))
+    #return data
+
+
 def process_form(form):
+
+    obj = ''
+
+    for key, value in form.to_dict().items():
+        obj += '{0}:  {1}<br>'.format(key, value)
+
+    return obj
+
     if form['ip_addr_type'] == 'A':
         if re.match(REGEX_ipv4, request.form['ip_address']):
             ip_address = form['ip_address']
 
         else:
-            ip_address = "Invalid ipv4 address"
+            return "Invalid ipv4 address"
     elif form['ip_addr_type'] == 'AAAA':
         if re.match(REGEX_ipv6, request.form['ip_address']):
             ip_address = form['ip_address']
         else:
-            ip_address = "Invalid ipv6 address"
+            return "Invalid ipv6 address"
 
     zone_doc = {
                 'domain': form['domain'],
@@ -49,12 +78,14 @@ def process_form(form):
                 'mail_host': form['mail_host'],
                 'mail_ip_host': form['mail_ip_host'],
                 'mail_priority': form['mail_priority'],
-                'ttl': form['ttl'],
                 'last_modify': datetime.datetime.utcnow()
                 }
+    if form['ttl'] != '':
+        zone_doc['ttl'] = form['ttl']
+
     collection.insert_one(zone_doc)
     return zone_doc
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,89 +1,95 @@
-let counterNsRecors = 2;
-let counterHostRecors = 2;
-let counterMailRecors = 2;
+let counterNsRecords = 2;
+let counterHostRecords = 2;
+let counterMailRecords = 2;
 
 $(document).ready(function(){
 
-	document.getElementById("defaultOpen").click();
+    document.getElementById("defaultOpen").click();
 
-	$(".external-check").change(ChangeEvent);
+    $(".external-check").change(ChangeEvent);
 
-	$("#add_ns").click(function(){
-		let content = `
+    $("#add_ns").click(function(){
+        let content = `
 		  	<div class='ns_record'>
-				<input type="text" name="ns${counterNsRecors}" id="ns${counterNsRecors}" placeholder="name">
-		        <select name="ns_ip_addr_type${counterNsRecors}" id="ns_ip_addr_type${counterNsRecors}">
+				<input type="text" name="ns${counterNsRecords}" id="ns${counterNsRecords}" placeholder="name">
+		        <select name="ns_ip_addr_type${counterNsRecords}" id="ns_ip_addr_type${counterNsRecords}">
 		        	<option disabled selected>IP record type</option>
 		            <option value="A">A</option>
 		        	<option value="AAAA">AAAA</option>
 		        </select >
-				<input type="text" name="ns_ip${counterNsRecors}" id="ns_ip${counterNsRecors}" placeholder="ip address">					
-				<input type="number" name="ns_ttl${counterNsRecors}" id="ns_ttl${counterNsRecors}" placeholder="time to live" min="0" max="1209600">
+				<input type="text" name="ns_ip${counterNsRecords}" id="ns_ip${counterNsRecords}" placeholder="ip address">					
+				<input type="number" name="ns_ttl${counterNsRecords}" id="ns_ttl${counterNsRecords}" placeholder="time to live" min="0" max="1209600">
 		  	</div>`;
-		$(".ns_record_wrapper").append(content);
-		counterNsRecors++;
-	});
+        $(".ns_record_wrapper").append(content);
+        counterNsRecords++;
+    });
 
-	$("#add_host").click(function(){
-		let content = `
+    $("#add_host").click(function(){
+        let content = `
 			<div class="host_record">
-				<input type="text" name="host_name${counterHostRecors}" id="host_name${counterHostRecors}" placeholder="host name">
-		        <select name="host_name_ip_addr_type${counterHostRecors}" id="host_name_ip_addr_type${counterHostRecors}">
+				<input type="text" name="host_name${counterHostRecords}" id="host_name${counterHostRecords}" placeholder="host name">
+		        <select name="host_name_ip_addr_type${counterHostRecords}" id="host_name_ip_addr_type${counterHostRecords}">
 		        	<option disabled selected>IP record type</option>
 		            <option value="A">A</option>
 		        	<option value="AAAA">AAAA</option>
 		        </select >
-				<input type="text" name="host_name_ip${counterHostRecors}" id="host_name_ip${counterHostRecors}" placeholder="ip address">					
-				<input type="number" name="host_name_ttl${counterHostRecors}" id="host_name_ttl${counterHostRecors}" placeholder="time to live" min="0" max="86400">
+				<input type="text" name="host_name_ip${counterHostRecords}" id="host_name_ip${counterHostRecords}" placeholder="ip address">					
+				<input type="number" name="host_name_ttl${counterHostRecords}" id="host_name_ttl${counterHostRecords}" placeholder="time to live" min="0" max="86400">
 			</div>
 		`;
-		$(".host_record_wrapper").append(content);
-		counterHostRecors++;
-	});
+        $(".host_record_wrapper").append(content);
+        counterHostRecords++;
+    });
 
-	$("#add_mail").click(function(){
-		let content = `
+    $("#add_mail").click(function(){
+        let content = `
 			<div class="mail_record">
-			    <input name="mail_host${counterMailRecors}" id="mail_host${counterMailRecors}" type="text" placeholder="mail host name">
-			    <input name="mail_ip_host${counterMailRecors}" id="mail_ip_host${counterMailRecors}" type="text" placeholder="mail host ip address">
-			    <input name="mail_preference${counterMailRecors}" id="mail_preference${counterMailRecors}" type="number" min="0" max="65535" placeholder="preference">
-			    <input type="number" name="mail_ttl${counterMailRecors}" id="mail_ttl${counterMailRecors}" placeholder="time to live" min="0" max="3024000">
+			    <input name="mail_host${counterMailRecords}" id="mail_host${counterMailRecords}" type="text" placeholder="mail host name">
+			    <input name="mail_ip_host${counterMailRecords}" id="mail_ip_host${counterMailRecords}" type="text" placeholder="mail host ip address">
+			    <input name="mail_preference${counterMailRecords}" id="mail_preference${counterMailRecords}" type="number" min="0" max="65535" placeholder="preference">
+			    <input type="number" name="mail_ttl${counterMailRecords}" id="mail_ttl${counterMailRecords}" placeholder="time to live" min="0" max="3024000">
                 <label class="checkbox-inline">
-                    <input type="checkbox" class="external-check  form-check-input" name="external${counterMailRecors}" value="true">External record
+                    <input type="checkbox" class="external-check  form-check-input" name="external${counterMailRecords}" value="true">External record
                 </label> 
 			</div>
 		`;
-		$(".mail_record_wrapper").append(content);
-		counterMailRecors++;
-		$('.external-check:last').change(ChangeEvent);
-	});
+        $(".mail_record_wrapper").append(content);
+        counterMailRecords++;
+        $('.external-check:last').change(ChangeEvent);
+    });
+
+    $('#btn-submit').click(sendFrom);
 
 });
 
-function openCity(evt, cityName) {
-	let i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-	tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-	tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(cityName).style.display = "block";
-	evt.currentTarget.className += " active";
+function openTab(evt, cityName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
 
 function ChangeEvent(){
-	let order_of_this = this.name.substring(8);	
-	let input_target = $('#mail_ip_host' + order_of_this)[0];
+    let order_of_this = this.name.substring(8);
+    let input_target = $('#mail_ip_host' + order_of_this)[0];
 
     if(this.checked){
         input_target.disabled = true;
         input_target.value = '';
     }
     else{
-    	input_target.disabled = false;
+        input_target.disabled = false;
     }
+}
+
+function sendFrom(){
+
 }
